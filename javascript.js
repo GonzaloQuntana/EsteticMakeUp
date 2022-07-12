9543fa9223bd2d370acf77a2842bcff611a2f660
Quintana Gonzalo <quintanagonzalodaniel@gmail.com>
2022-07-12 19:30:49 -0300

    JavaScript

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function bienvenida(){
+
+    console.log("Bienvenid@ a Estetic Make Up");
+
+}
+
+bienvenida();
+
+let producto = prompt("Ingrese el nombre del producto");
+let precio = parseInt(prompt("Ingrese el precio del producto"));
+
+console.log("--------------------------")
+console.log("Nombre:" , producto);
+console.log("Precio" , precio);
