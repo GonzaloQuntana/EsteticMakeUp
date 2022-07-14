7349ddf0701755f3ba5a9809b9fe4d64a51149a7
Quintana Gonzalo <quintanagonzalodaniel@gmail.com>
2022-07-14 14:54:28 -0300

    Primera entrega proyecto final

--- javascript.js
+++ javascript.js
@@ -6,9 +6,45 @@ function bienvenida(){
 
 bienvenida();
 
-let producto = prompt("Ingrese el nombre del producto");
-let precio = parseInt(prompt("Ingrese el precio del producto"));
+let productos = [
+    {nombre:"Labial" , precio:100},
+    {nombre:"Sombra" , precio:200},
+    {nombre:"Pestañas" , precio:50},
+    {nombre:"Uñas" , precio:150}
+]
 
-console.log("--------------------------")
-console.log("Nombre:" , producto);
-console.log("Precio" , precio);
+function productos_venta( producto ){
+
+    return {
+        nombre: producto.nombre,
+        precio: producto.precio
+    }
+}
+
+let resultado_map = productos.map ( productos_venta );
+console.log(resultado_map)
+
+function compra1(){
+
+    console.log("Pepe realizo una ocmpra");
+
+}
+
+compra1();
+
+let ventas = [
+    {nombre:"Labial" , precio:100},
+    {nombre:"Sombra" , precio:200},
+    {nombre:"Pestañas" , precio:50},
+    {nombre:"Uñas" , precio:150}
+]
+
+function calcular_total( acu , producto ){
+
+    acu = acu + producto.precio;
+
+    return acu
+}
+
+let venta_total = ventas.reduce( calcular_total , 0);
+console.log("El total es:" , venta_total);
